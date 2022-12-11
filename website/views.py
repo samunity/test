307c2434dcf90a471a90086cbbc1a06953e7e061
saeed <amuyesad@gmail.com>
2022-12-11 12:57:08 +0330

    added render html
    added html files and render it

--- website/views.py
+++ website/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
 
 def home_view(request):
-    return HttpResponse('<h1>Home Page</h1>')
+    return render(request,'website/home.html')
 
 def about_view(request):
-    return HttpResponse('<h1>About Page</h1>')
+    return render(request,'website/about.html')
 
 def contact_view(request):
-    return HttpResponse('<h1>Contact Page</h1>')
+    return render(request,'website/contact.html')
